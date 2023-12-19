1557b527dcf6b449742f4f33920ae2da4e587091
Derek Walton <up2124866@myport.ac.uk>
2023-12-19 19:07:32 +0000

    Plan added for ideas / blueprint

---
+++ ShortsBotPlan.py
@@ -0,0 +1,34 @@
+# INPUTS
+
+    # link for background video
+        # Subway surfers, minecraft parkour, gta racing, satisfying videos etc
+            # randomly use minute long sections of the video
+
+    # link for reddit story
+        # in future make it compatible for family guy, south park, simpsons, podcasts (joe rogan etc), twitch clips (caseoh etc), movie clips
+
+# ALTERNATIVE INPUTS
+
+    # Could be even simplier
+
+    # inputs: screen shot of title, copy and paste of text
+
+
+
+# PROCESS
+
+    # Put background video behind everything
+
+    # Take screenshot of the title of the thread / message and reads out the title
+
+    # Speaks every word and Prints every word individually as they are spoken
+
+    # If video exceeds 1 minute, make a part 2
+
+
+
+# OUTPUT
+
+    # 1 - 4 Minute ish long clips for stories 
+
+    # Still image of the starting frame including the part number
