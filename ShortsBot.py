9548ab92ad360bb863d3db1432bc75b2f321960b
Derek Walton <up2124866@myport.ac.uk>
2023-12-19 22:55:46 +0000

    read and write video

--- ShortsBot.py
+++ ShortsBot.py
@@ -4,4 +4,4 @@ from moviepy.editor import *
 clip = VideoFileClip("stock-background-videos/test-video.mp4", audio=False)
 
 # downloads video clip
-clip.write_videofile("new-test-video")
+clip.write_videofile("output-video/new-test-video.mp4")
