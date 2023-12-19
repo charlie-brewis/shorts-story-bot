67288db2678ada5201ce12f6b69d388f7d3f85c6
Derek Walton <up2124866@myport.ac.uk>
2023-12-19 23:16:42 +0000

    tts impliment

--- ShortsBot.py
+++ ShortsBot.py
@@ -1,7 +1,27 @@
 from moviepy.editor import *
+import pyttsx3
 
-# access to video
-clip = VideoFileClip("stock-background-videos/test-video.mp4", audio=False)
+def convert_script_to_audio(script: str) -> None:
+    engine = pyttsx3.init()
+    engine.save_to_file(script, "test.mp3")
+    engine.runAndWait()
+    engine.close()
 
-# downloads video clip
-clip.write_videofile("output-video/new-test-video.mp4")
+def generate_background_video() -> None:
+    # file location for background video
+    clip = VideoFileClip("stock-background-videos/test-video.mp4", audio=False)
+    
+    
+    start = 0
+    # cuts video from 1st paramter to second paramter (in seconds)
+    clip = clip.subclip(start, start + 60)
+    
+    # choose where to download output video
+    clip.write_videofile("output-video/new-test-video.mp4")
+
+
+def main() -> None:
+    plain_text = input("Enter script: ")
+    convert_script_to_audio(plain_text)
+    print("E")
+     
